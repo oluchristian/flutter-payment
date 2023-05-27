b7f6d4309e7488867dfcf9f651967e058a89375a
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-27 19:03:20 +0100

    new commit

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -21,20 +21,139 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Stack(
           children: [
             _headSection(),
-
+            _listBills(),
           ],
         ),
       ),
     );
   }
-  _headSection(){
+
+  _headSection() {
     return Container(
       height: 310,
-      
       child: Stack(
         children: [
-
+          _mainBackground(),
+          _curvedImageContainer(),
+          _buttonContainer(),
         ],
+      ),
+    );
+  }
+
+  _buttonContainer() {
+    return Positioned(
+      right: 58,
+      bottom: 15,
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/lines.png'),
+          ),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 15,
+              offset: Offset(0, 1),
+              color: Color(0xFF11324D).withOpacity(0.2),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _mainBackground() {
+    return Positioned(
+      bottom: 10,
+      left: 0,
+      child: Container(
+        height: 300,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 3,
+            color: Colors.grey
+          ),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'images/background.png',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _curvedImageContainer() {
+    return Positioned(
+      left: 0,
+      right: -2,
+      bottom: 10,
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.1,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'images/curve.png',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _listBills() {
+    return Positioned(
+      top: 320,
+      child: Container(
+        height: 130,
+        width: MediaQuery.of(context).size.width - 20,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFdbdbe0),
+              offset: Offset(1, 1),
+              blurRadius: 20.0,
+              spreadRadius: 10,
+            ),
+          ],
+        ),
+        child: Container(
+          margin: const EdgeInsets.only(top: 10, left: 18),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                              'images/brand1.png',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
