9d60238d12a50c582e518d8c5fc45ea96d1c8fee
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-28 08:09:39 +0100

    added modal button

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_payment_app/component/colors.dart';
+import 'package:flutter_payment_app/widgets/large_buttons.dart';
+import 'package:flutter_payment_app/widgets/text_size.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -22,6 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             _headSection(),
             _listBills(),
+            _payButton(),
           ],
         ),
       ),
@@ -45,20 +48,45 @@ class _MyHomePageState extends State<MyHomePage> {
     return Positioned(
       right: 58,
       bottom: 15,
-      child: Container(
-        height: 60,
-        width: 60,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/lines.png'),
-          ),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 15,
-              offset: Offset(0, 1),
-              color: Color(0xFF11324D).withOpacity(0.2),
+      child: GestureDetector(
+        onTap: (){
+          showModalBottomSheet<dynamic>(
+            isScrollControlled: true,
+            barrierColor: Colors.transparent,
+            backgroundColor: Colors.grey.withOpacity(0.5),
+            context: context, builder: (BuildContext bc){
+            return Container(
+              height: MediaQuery.of(context).size.height-240,
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                    
+                    color: Colors.red,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height-300,
+                  )),
+                ],
+              ),
+            );
+          });
+        },
+        child: Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/lines.png'),
             ),
-          ],
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 15,
+                offset: Offset(0, 1),
+                color: Color(0xFF11324D).withOpacity(0.2),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -72,10 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
         height: 300,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-          border: Border.all(
-            width: 3,
-            color: Colors.grey
-          ),
+          border: Border.all(width: 3, color: Colors.grey),
           image: DecorationImage(
             fit: BoxFit.cover,
             image: AssetImage(
@@ -109,52 +134,175 @@ class _MyHomePageState extends State<MyHomePage> {
   _listBills() {
     return Positioned(
       top: 320,
-      child: Container(
-        height: 130,
-        width: MediaQuery.of(context).size.width - 20,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30),
-            bottomRight: Radius.circular(30),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Color(0xFFdbdbe0),
-              offset: Offset(1, 1),
-              blurRadius: 20.0,
-              spreadRadius: 10,
-            ),
-          ],
-        ),
-        child: Container(
-          margin: const EdgeInsets.only(top: 10, left: 18),
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        height: 60,
-                        width: 60,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(
-                              'images/brand1.png',
-                            ),
-                          ),
-                        ),
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: ListView.builder(
+          itemCount: 3,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(top: 20, right: 20),
+                  height: 130,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFFdbdbe0),
+                        offset: Offset(1, 1),
+                        blurRadius: 20.0,
+                        spreadRadius: 10,
                       ),
                     ],
                   ),
-                ],
-              ),
-            ],
-          ),
-        ),
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10, left: 18),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  height: 60,
+                                  width: 60,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(
+                                      width: 3,
+                                      color: Colors.grey,
+                                    ),
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                        'images/brand1.png',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'KenGen Power',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: AppColor.mainColor,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'ID:846594',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: AppColor.idColor,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedText(
+                              text: "Auto pay on 24th May 18",
+                              color: AppColor.green,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 80,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: AppColor.selectBackground,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      'Select',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: AppColor.selectColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(),
+                                ),
+                                Text(
+                                  '\$1248.00',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w900,
+                                    color: AppColor.mainColor,
+                                  ),
+                                ),
+                                Text(
+                                  'Due in 3 days',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                    color: AppColor.idColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Container(
+                              width: 5,
+                              height: 35,
+                              decoration: BoxDecoration(
+                                color: AppColor.halfOval,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(30),
+                                  bottomLeft: Radius.circular(30),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
       ),
-    );
+      );
+    
+  }
+
+  _payButton(){
+    return Positioned(
+      bottom: 20,
+      child: AppLargeButton(text: 'Pay all bills', textColor: Colors.white,),);
   }
 }
