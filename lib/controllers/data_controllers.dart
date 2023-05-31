0a1b1c7105103dcebc12a3638f64e869cdb97ebb
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-05-31 17:13:28 +0100

    new

--- lib/controllers/data_controllers.dart
+++ lib/controllers/data_controllers.dart
@@ -3,15 +3,12 @@ import 'package:get/get.dart';
 
 class DataController extends GetxController{
   var list = [].obs;
-  var test = [].obs;
   final service = new DataServices();
   @override
   void onInit() {
     _loadData();
     super.onInit();
   }
-
-
 
    _loadData() async {   
     var info = await service.getUsers();
